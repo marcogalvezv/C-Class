5d2d2b722c62ddcfae9ce73dfa49de57e8fb0929
mgalvez-aptos-com <mgalvez@aptos.com>
2018-04-24 15:50:57 -0400

    adding new project ParsingValuesFromStringData

---
+++ MainMethod/MainMethod/ParsingValuesfromStringData/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingValuesfromStringData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParseFromStrings();
+            ParseFromStringsWithTryParse();
+        }
+
+        static void ParseFromStrings()
+        {
+            Console.WriteLine("=> Data type parsing:");
+            bool b = bool.Parse("True");
+            Console.WriteLine("Value of b: {0}", b);
+            double d = double.Parse("99.884");
+            Console.WriteLine("Value of d: {0}", d);
+            int i = int.Parse("8");
+            Console.WriteLine("Value of i: {0}", i);
+            char c = Char.Parse("w");
+            Console.WriteLine("Value of c: {0}", c);
+            Console.WriteLine();
+        }
+
+
+        static void ParseFromStringsWithTryParse()
+        {
+            Console.WriteLine("=> Data type parsing with TryParse:");
+            if (bool.TryParse("True", out bool b)) ;
+            {
+                Console.WriteLine("Value of b: {0}", b);
+            }
+            string value = "Hello";
+            if (double.TryParse(value, out double d))
+            {
+                Console.WriteLine("Value of d: {0}", d);
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert the input ({0}) to a double", value);
+            }
+            Console.WriteLine();
+        }
+    }
+}
