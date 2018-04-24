aa78b5831bd04bd1fbc1ea98ff1b212deeb4d695
mgalvez-aptos-com <mgalvez@aptos.com>
2018-04-24 15:08:53 -0400

    Adding changes for MainMethod Project

--- MainMethod/MainMethod/MainMethod/Program.cs
+++ MainMethod/MainMethod/MainMethod/Program.cs
@@ -4,9 +4,25 @@ namespace MainMethod
 {
     class Program
     {
-        static void Main(string[] args)
+
+        #region Variations on the Main( ) Method
+        //// int return type, array of strings as the parameter.
+        //static int Main(string[] args)
+        //{
+        //    // Must return a value before exiting!
+        //    return 0;
+        //}
+        //// No return type, no parameters.
+        //static void Main()
+        //{
+        //}
+        //// int return type, no parameters.
+        static int Main()
         {
-            Console.WriteLine("Hello World!");
+            // Must return a value before exiting!
+            return 0;
         }
+        #endregion
+
     }
 }
