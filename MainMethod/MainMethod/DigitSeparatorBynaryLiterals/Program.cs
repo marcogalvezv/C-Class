409772d2e49b827eb23b66919df05642542e4d39
mgalvez-aptos-com <mgalvez@aptos.com>
2018-04-24 16:18:27 -0400

    Adding Digitseparator and NewingDataTypes

---
+++ MainMethod/MainMethod/DigitSeparatorBynaryLiterals/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitSeparatorBynaryLiterals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitSeparators();
+            BinaryLiterals();
+        }
+
+        static void DigitSeparators()
+        {
+            Console.WriteLine("=> Use Digit Separators:");
+            Console.Write("Integer:");
+            Console.WriteLine(123_456);
+            Console.Write("Long:");
+            Console.WriteLine(123_456_789L);
+            Console.Write("Float:");
+            Console.WriteLine(123_456.1234F);
+            Console.Write("Double:");
+            Console.WriteLine(123_456.12);
+            Console.Write("Decimal:");
+            Console.WriteLine(123_456.12M);
+        }
+
+        private static void BinaryLiterals()
+        {
+            Console.WriteLine("=> Use Binary Literals:");
+            Console.WriteLine("Sixteen: {0}", 0b0001_0000);
+            Console.WriteLine("Thirty Two: {0}", 0b0010_0000);
+            Console.WriteLine("Sixty Four: {0}", 0b0100_0000);
+        }
+    }
+}
---
+++ MainMethod/MainMethod/NewingDataTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewingDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewingDataTypes();
+        }
+
+        static void DefaultDeclarations()
+        {
+            Console.WriteLine("=> Default Declarations:");
+            int myInt = default;
+        }
+        static void NewingDataTypes()
+        {
+            Console.WriteLine("=> Using new to create variables:");
+            bool b = new bool(); // Set to false.
+            int i = new int(); // Set to 0.
+            double d = new double(); // Set to 0.
+            DateTime dt = new DateTime(); // Set to 1/1/0001 12:00:00 AM
+            Console.WriteLine("{0}, {1}, {2}, {3}", b, i, d, dt);
+            Console.WriteLine();
+        }
+    }
+}
