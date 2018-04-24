be300e7c9fdadb793cef04ed53a4c073e02a16da
mgalvez-aptos-com <mgalvez@aptos.com>
2018-04-24 15:14:09 -0400

    Adding Code to process args on a consoleApplication

--- MainMethod/MainMethod/MainMethod/Program.cs
+++ MainMethod/MainMethod/MainMethod/Program.cs
@@ -17,12 +17,22 @@ namespace MainMethod
         //{
         //}
         //// int return type, no parameters.
-        static int Main()
-        {
-            // Must return a value before exiting!
-            return 0;
-        }
+        //static int Main()
+        //{
+        //    // Must return a value before exiting!
+        //    return 0;
+        //}
         #endregion
 
+        #region incoming args
+        static int Main(string[] args)
+        {
+            // Process any incoming args.
+            for (int i = 0; i < args.Length; i++)
+                Console.WriteLine("Arg: {0}", args[i]);
+            Console.ReadLine();
+            return -1;
+        }
+        #endregion
     }
 }
