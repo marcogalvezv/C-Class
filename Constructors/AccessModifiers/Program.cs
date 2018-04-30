554b294683e6fee539a77ef47a2cee78639aeeef
mgalvez-aptos-com <mgalvez@aptos.com>
2018-04-30 19:18:27 -0400

    Access Modifiers Code

---
+++ Constructors/AccessModifiers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    
+    //Default Access Modifier
+    // An internal class with a private default constructor.
+    class Radio
+    {
+        Radio() { }
+    }
+
+
+    //// An internal class with a private default constructor.
+    //internal class Radio
+    //{
+    //    private Radio() { }
+    //}
+
+    //// A public class with a public default constructor.
+    //public class Radio
+    //{
+    //    public Radio() { }
+    //}
+
+
+    //Access Modifiers and Nested Types
+
+    public class SportsCar
+    {
+        // OK! Nested types can be marked private.
+        private enum CarColor
+        {
+            Red, Green, Blue
+        }
+    }
+}
