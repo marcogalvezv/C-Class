b80a1fdc7707c75e6c79b942c0faad0d36f29021
mgalvez-aptos-com <mgalvez@aptos.com>
2018-05-04 19:28:56 -0400

    Adding GenericsBasicExample

---
+++ ArrayExample/GenericsIntro/Check.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class Check<DATATYPE>
+    {
+        //public bool Compare(int i, int y)
+        //{
+        //    if (i == y)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        public bool Compare(DATATYPE i, DATATYPE y)
+        {
+            if (i.Equals(y))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ArrayExample/GenericsIntro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check<int> c = new Check<int>();
+            var r = c.Compare(5, 6);
+
+            Console.WriteLine(r);
+
+            Check<string> sc = new Check<string>();
+            var sr = sc.Compare("TEST", "TEST");
+            Console.WriteLine(sr);
+
+            Console.ReadLine();
+
+        }
+    }
+}
