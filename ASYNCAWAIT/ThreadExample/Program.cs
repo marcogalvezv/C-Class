4f4ad886e64e1528c84a2bfb3eac88401302bf2c
mgalvez-aptos-com <mgalvez@aptos.com>
2018-05-18 19:03:45 -0400

    Adding ThreadExample

---
+++ ASYNCAWAIT/ThreadExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread o1 = new Thread(RunMillionIterarions);
+            o1.Start();
+            //Parallel.For(0, 1000000, x => RunMillionIterarions());
+            Console.Read();
+        }
+
+        private static void RunMillionIterarions()
+        {
+            string x = "";
+            for (int i = 0; i < 1000000; i++)
+            {
+                x = x + "s";
+            }
+        }
+    }
+}
