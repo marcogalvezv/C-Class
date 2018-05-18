ffd250fd93872417dba07fcf2e3062860d23f4c9
mgalvez-aptos-com <mgalvez@aptos.com>
2018-05-18 18:44:23 -0400

    Adding AsyncAwait

---
+++ ASYNCAWAIT/ASYNCAWAIT/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ASYNCAWAIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method();
+            Console.WriteLine("Main Thread");
+            Console.ReadLine();
+        }
+
+        private static async void Method()
+        {
+            await Task.Run(new Action(LongTask));
+            Console.WriteLine("New Thread");
+        }
+
+        private static void LongTask()
+        {
+            Thread.Sleep(20000);
+        }
+    }
+}
